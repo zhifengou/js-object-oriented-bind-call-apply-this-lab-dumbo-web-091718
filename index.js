ccc88999b8ad0095382bf83a0d9360e1bf4b2c8a
zhifeng ou <zhifengou@hotmail.com>
2019-03-15 18:06:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Your code here
+function justInvoke(fn){
+    return fn();
+}
+ function setThisWithCall(fn, thisValue, arg){
+    return fn.call(thisValue, arg);
+}
+ function setThisWithApply(fn, thisValue, args){
+    return fn.apply(thisValue, args);
+}
+ function returnNewFunctionOf(functionToBeCopied, thisValue) {
+    return functionToBeCopied.bind(thisValue);
+}
